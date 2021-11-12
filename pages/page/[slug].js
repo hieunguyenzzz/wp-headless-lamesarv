ae0e0206ca8ece83d3dd618eee0d4f035ad440b0
Hieu Nguyen <hieunguyenel@gmail.com>
2021-11-12 21:57:43 +0700

    update

--- pages/page/[slug].js
+++ pages/page/[slug].js
@@ -1,8 +1,14 @@
 import PageHomepage from 'components/PageHomepage';
+import PageSearch from 'components/PageSearch';
+import { useRouter } from 'next/router';
 import cookedData from '../../data/cookedData.json';
 import { getHomePageProps } from '../../libs/utils/pageProps';
 
 export default function Home({ pageProps }) {
+    const router = useRouter();
+    if (router.query.s) {
+        return <PageSearch pageProps={pageProps} />;
+    }
     return <PageHomepage pageProps={pageProps} />;
 }
 
